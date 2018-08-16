dc6eb9361a09bbdc0a344a0dfe218c50f1ad190a
Alex Maingot <amaingot@spredfast.com>
2018-08-16 17:48:48 -0500

    Transform Badge

--- packages/tsg-front/src/components/Badge.tsx
+++ packages/tsg-front/src/components/Badge.tsx
@@ -1,19 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import { WithTheme } from '@material-ui/core';
-import withStyles from '@material-ui/core/styles/withStyles';
-
-import BadgeStyle from 'styles/BadgeStyle';
 import { Color } from 'styles/Theme';
 
+const StyledBadge = styled.span`
+  margin-right: 3px;
+  border-radius: 12px;
+  padding: 5px 12px;
+  text-transform: uppercase;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 1;
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+  display: inline-block;
+
+  .primary {
+    background-color: ${Color.primary};
+  }
+  .warning {
+    background-color: ${Color.warning};
+  }
+  .danger {
+    background-color: ${Color.danger};
+  }
+  .success {
+    background-color: ${Color.success};
+  }
+  .info {
+    background-color: ${Color.info};
+  }
+  .rose {
+    background-color: ${Color.rose};
+  }
+  .gray {
+    background-color: #6c757d;
+  }
+`;
+
 interface BadgeProps {
-  classes: Record<string, string>;
   color?: Color;
+  children?: React.ReactNode;
 }
 
-const Badge: React.SFC<BadgeProps & Partial<WithTheme>> = props => {
-  const { classes, color, children } = props;
-  return <span className={classes.badge + ' ' + classes[color || 'gray']}>{children}</span>;
+const Badge: React.SFC<BadgeProps> = props => {
+  const { color, children } = props;
+  return <StyledBadge className={color || 'gray'}>{children}</StyledBadge>;
 };
 
-export default withStyles(BadgeStyle)(Badge);
+export default Badge;
