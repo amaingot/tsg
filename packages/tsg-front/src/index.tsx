602fe40ca5827ece9cecf37b64caa180b547e286
Alex Maingot <alex.maingot@gmail.com>
2018-12-27 14:15:32 -0600

    Adding sentry

--- packages/tsg-front/src/index.tsx
+++ packages/tsg-front/src/index.tsx
@@ -1,7 +1,7 @@
+import * as Sentry from '@sentry/browser';
 import { createBrowserHistory } from 'history';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
 import configureStore from './configureStore';
 
 import Main from './main';
@@ -10,6 +10,11 @@ const history = createBrowserHistory();
 
 const initialState = window.initialReduxState;
 const store = configureStore(history, initialState);
+
+Sentry.init({
+  dsn: 'https://6b4dbd70df3a4cee8ce931a9cdfa87ac@sentry.io/1263890',
+  environment: App.env,
+});
 
 ReactDOM.render(
   <Main store={store} history={history} />,
