4c821c44f56d4929f76b411363e756915a765295
Alex Maingot <alex.maingot@gmail.com>
2019-10-03 00:52:28 -0500

    Adding rollbar sugar

--- api/handlers/utils/withRollbar.ts
+++ api/handlers/utils/withRollbar.ts
@@ -9,7 +9,18 @@ import * as Rollbar from "rollbar";
 import AWSXRay from "aws-xray-sdk-core";
 
 const rollbar = new Rollbar({
-  accessToken: process.env.LAMBDA_ROLLBAR_TOKEN
+  accessToken: process.env.LAMBDA_ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  reportLevel: "warning",
+  payload: {
+    environment: process.env.ENV,
+    client: {
+      javascript: {
+        code_version: process.env.CODE_VERSION
+      }
+    }
+  }
 });
 type RollbarEnhancer = (
   handler: APIGW
@@ -26,6 +37,17 @@ const withRollbar: RollbarEnhancer = (handler: APIGW) => async (
 ) => {
   try {
     AWSXRay.setLogger(rollbar);
+
+    if (event.requestContext.authorizer.claims) {
+      rollbar.configure({
+        payload: {
+          person: {
+            id: event.requestContext.authorizer.claims["cognito:username"],
+            email: event.requestContext.authorizer.claims["email"]
+          }
+        }
+      });
+    }
 
     rollbar.log("Received event:", JSON.stringify(event, null, 2));
     return await handler(event, context, callback);
