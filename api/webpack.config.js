b0664d2f59f76a6f0137c5a0effb9ad51e914002
Alex Maingot <alex.maingot@gmail.com>
2019-10-06 15:52:38 -0500

    Fix deploy

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,16 +1,17 @@
 const path = require("path");
 const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
 
-const layerPackageJson = require("./layer/package.json");
-const layerExternals = Object.keys(layerPackageJson.dependencies).reduce(
-  (result, currentDep) => {
-    return {
-      ...result,
-      [currentDep]: currentDep
-    };
-  },
-  {}
-);
+// const layerPackageJson = require("./layer/package.json");
+// const layerExternals = Object.keys(layerPackageJson.dependencies).reduce(
+//   (result, currentDep) => {
+//     return {
+//       ...result,
+//       [currentDep]: currentDep
+//     };
+//   },
+//   {}
+// );
 
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? "development" : "production",
@@ -24,12 +25,12 @@ module.exports = {
     path: path.join(__dirname, ".webpack"),
     filename: "[name].js"
   },
-  target: "node",
+  target: "node", // in order to ignore built-in modules like path, fs, etc.
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: "ts-loader" }
     ]
   },
-  externals: slsw.lib.webpack.isLocal ? {} : layerExternals
+  externals: [nodeExternals()] // in order to ignore all modules in node_modules folder
 };
