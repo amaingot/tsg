0ec0707ffa41eae72504b18f8801a8af0bc4bced
Alex Maingot <alex.maingot@gmail.com>
2019-10-21 14:03:54 -0500

    Fix unused typography

--- web/src/components/AppNavBar.tsx
+++ web/src/components/AppNavBar.tsx
@@ -5,7 +5,6 @@ import { NavLink } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
