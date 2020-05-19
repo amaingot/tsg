e35c4154681ad7b09c78f5cbfa05da81ba633531
Alex Maingot <alex.maingot@gmail.com>
2020-05-19 13:51:06 -0500

    Add so many good helpers

--- server/src/db/helpers.ts
+++ server/src/db/helpers.ts
@@ -1,35 +1,71 @@
+import { buildPaginator } from "typeorm-cursor-pagination";
 import {
   ObjectType,
   getConnection,
   DeepPartial,
   SelectQueryBuilder,
 } from "typeorm";
-import { buildPaginator } from "typeorm-cursor-pagination";
-import { PaginationInput } from "../graphql/types";
-import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
 
-interface Filter<Entity> {
-  alias: string;
-  query: (qb: SelectQueryBuilder<Entity>) => SelectQueryBuilder<Entity>;
+import { PaginationInput } from "../graphql/types";
+import { GraphqlContext } from "../graphql/context";
+import { logger } from "../utils/logger";
+import { AuthenticationError } from "apollo-server-express";
+
+import { Client } from "./entities/Client";
+import { Customer } from "./entities/Customer";
+import { Job } from "./entities/Job";
+import { Employee } from "./entities/Employee";
+
+type ApplicationEntity = Client | Customer | Job | Employee;
+
+type ProtectedQuery<Entity extends ApplicationEntity> = (
+  context: GraphqlContext
+) => (qb: SelectQueryBuilder<Entity>) => SelectQueryBuilder<Entity>;
+
+const getProtectedQuery = <Entity extends ApplicationEntity>(
+  target: ObjectType<Entity>
+) => {
+  switch (target.name) {
+    case Client.name:
+      return (Client.protectedQuery as any) as ProtectedQuery<Entity>;
+
+    case Employee.name:
+      return (Employee.protectedQuery as any) as ProtectedQuery<Entity>;
+
+    case Customer.name:
+      return (Customer.protectedQuery as any) as ProtectedQuery<Entity>;
+
+    case Job.name:
+      return (Job.protectedQuery as any) as ProtectedQuery<Entity>;
+    default:
+      throw new Error();
+  }
+};
+
+const defaultQuery = <Entity>() => (qb: SelectQueryBuilder<Entity>) => qb;
+
+interface FindManyArgs<Entity extends ApplicationEntity> {
+  target: ObjectType<Entity>;
+  input?: PaginationInput;
+  context: GraphqlContext;
+  query?: (qb: SelectQueryBuilder<Entity>) => SelectQueryBuilder<Entity>;
 }
 
-const defaultFilter = <Entity>() => ({
-  alias: "row",
-  query: (qb: SelectQueryBuilder<Entity>) => qb,
-});
-
-export const findMany = async <Entity>(
-  target: ObjectType<Entity>,
-  input: PaginationInput = {},
-  filter?: Filter<Entity>
+export const findMany = async <Entity extends ApplicationEntity>(
+  args: FindManyArgs<Entity>
 ) => {
+  const { input = {}, context, target, query = defaultQuery<Entity>() } = args;
+
+  const protectedQuery = getProtectedQuery<Entity>(target)(context);
+
   const limit = input.limit || 100;
   const order = input.order || "ASC";
   const { cursor: cursorKey, type } = input.cursor || {};
-  const { alias, query } = filter || defaultFilter<Entity>();
 
   const queryBuilder = query(
-    getConnection().getRepository<Entity>(target).createQueryBuilder(alias)
+    protectedQuery(
+      getConnection().getRepository<Entity>(target).createQueryBuilder()
+    )
   );
 
   const paginator = buildPaginator<Entity>({
@@ -53,42 +89,125 @@ export const findMany = async <Entity>(
   };
 };
 
-export const findOne = <Entity>(target: ObjectType<Entity>, id?: string) => {
+interface FindOneArgs<Entity extends ApplicationEntity> {
+  target: ObjectType<Entity>;
+  id?: string;
+  context: GraphqlContext;
+}
+
+export const findOne = async <Entity extends ApplicationEntity>(
+  input: FindOneArgs<Entity>
+) => {
+  const { target, id, context } = input;
+
   if (id === undefined) return undefined;
-  return getConnection().getRepository(target).findOne(id);
+  const item = await getConnection().getRepository(target).findOne(id);
+  if (!item || !item.canAccess(context)) return undefined;
+
+  return item;
 };
 
-export const findOneOrFail = async <Entity>(
-  target: ObjectType<Entity>,
-  id: string
+export const findOneOrFail = async <Entity extends ApplicationEntity>(
+  input: FindOneArgs<Entity>
 ) => {
-  const record = await findOne(target, id);
+  const { target, id, context } = input;
+
+  const record = await findOne({ target, id, context });
   if (record === undefined) throw new Error();
   return record;
 };
 
-export const deleteOne = async <Entity>(
-  target: ObjectType<Entity>,
-  id: string
+interface DeleteOneArgs<Entity extends ApplicationEntity> {
+  target: ObjectType<Entity>;
+  id: string;
+  context: GraphqlContext;
+}
+
+export const deleteOne = async <Entity extends ApplicationEntity>(
+  input: DeleteOneArgs<Entity>
 ) => {
-  await getConnection().getRepository(target).softDelete(id);
+  const { target, id, context } = input;
+  const item = await getConnection().getRepository(target).findOne(id);
+  if (!item || !item.canDelete(context)) return false;
+
+  try {
+    await getConnection().getRepository(target).softDelete(id);
+  } catch (e) {
+    logger.error("Error deleting entity", { target, id, context });
+    return false;
+  }
   return true;
 };
 
-export const createOne = <Entity>(
-  target: ObjectType<Entity>,
-  input: DeepPartial<Entity>
+const getCanCreate = <Entity extends ApplicationEntity>(
+  target: ObjectType<Entity>
 ) => {
-  return getConnection().getRepository(target).create(input);
+  switch (target.name) {
+    case Client.name:
+      return Client.canCreate;
+
+    case Employee.name:
+      return Employee.canCreate;
+
+    case Customer.name:
+      return Customer.canCreate;
+
+    case Job.name:
+      return Job.canCreate;
+    default:
+      throw new Error();
+  }
 };
 
-export const updateOne = async <Entity>(
-  target: ObjectType<Entity>,
-  id: string,
-  input: QueryDeepPartialEntity<Entity>
+interface CreateOneArgs<Entity extends ApplicationEntity> {
+  target: ObjectType<Entity>;
+  item: DeepPartial<Entity>;
+  context: GraphqlContext;
+}
+
+export const createOne = <Entity extends ApplicationEntity>(
+  args: CreateOneArgs<Entity>
 ) => {
-  await getConnection().getRepository(target).update(id, input);
-  const item = await findOne(target, id);
-  if (item === undefined) throw new Error();
-  return item;
+  const { item, target, context } = args;
+
+  const canCreate = getCanCreate<Entity>(target);
+
+  if (!canCreate(context, item as any)) {
+    logger.warn("User trying to create an item they can't", {
+      target,
+      item,
+      context,
+    });
+    throw new AuthenticationError("You can't do this");
+  }
+  return getConnection().getRepository(target).create(item);
+};
+
+interface UpdateOneArgs<Entity extends ApplicationEntity> {
+  target: ObjectType<Entity>;
+  id: string;
+  updatedItem: DeepPartial<Entity>;
+  context: GraphqlContext;
+}
+
+export const updateOne = async <Entity extends ApplicationEntity>(
+  args: UpdateOneArgs<Entity>
+) => {
+  const { updatedItem, target, context, id } = args;
+
+  const oldItem = await findOneOrFail({ target, context, id });
+
+  if (!oldItem.canUpdate(context)) {
+    logger.warn("User trying to update item they are not allowed to", {
+      context,
+      target,
+      id,
+      updatedItem,
+      oldItem,
+    });
+    throw new AuthenticationError("Not authorized");
+  }
+
+  await getConnection().getRepository(target).update(id, updatedItem);
+  return findOneOrFail({ target, id, context });
 };
