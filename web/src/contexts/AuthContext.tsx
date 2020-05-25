35fdc1994470b4eee65393e1d82778ca1502505a
Alex Maingot <alex.maingot@gmail.com>
2020-05-25 15:41:04 -0500

    Add domain info to auth context

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import auth, { User } from "../utils/auth";
+import { Employee, Client, useGetMeLazyQuery } from "../graphql/hooks";
+
+type EmployeeRecord = Pick<
+  Employee,
+  | "id"
+  | "clientId"
+  | "firstName"
+  | "lastName"
+  | "email"
+  | "cellPhone"
+  | "userRole"
+  | "updatedAt"
+  | "createdAt"
+>;
+
+type ClientRecord = Pick<
+  Client,
+  "id" | "updatedAt" | "createdAt" | "name" | "stripeCustomerId"
+>;
 
 interface AuthContextState {
   loggedIn: boolean;
   loading: boolean;
   user?: User;
+  employee?: EmployeeRecord;
+  client?: ClientRecord;
 }
 
 const AuthContext = React.createContext<AuthContextState>({
@@ -15,23 +36,39 @@ const AuthContext = React.createContext<AuthContextState>({
 export const useAuth = () => React.useContext(AuthContext);
 
 export const AuthContextProvider: React.FC = (props) => {
+  const [getMe, meResult] = useGetMeLazyQuery();
+
   const [user, setUser] = React.useState<User>();
-  const [loading, setLoading] = React.useState(true);
+  const [employee, setEmployee] = React.useState<EmployeeRecord>();
+  const [client, setClient] = React.useState<ClientRecord>();
+  const [authLoading, setAuthLoading] = React.useState(true);
 
   React.useEffect(() => {
     auth.onAuthStateChanged((u) => {
       if (u) {
         setUser(u);
+        getMe();
       } else {
         setUser(undefined);
+        setEmployee(undefined);
+        setClient(undefined);
       }
-      setLoading(false);
+      setAuthLoading(false);
     });
-  }, []);
+  }, [getMe]);
+
+  React.useEffect(() => {
+    if (meResult.data?.me?.client) {
+      setEmployee(meResult.data.me);
+      setClient(meResult.data.me.client);
+    }
+  }, [meResult]);
+
+  const loading = authLoading || meResult.loading;
 
   return (
     <AuthContext.Provider
-      value={{ loading, loggedIn: user !== undefined, user }}
+      value={{ loading, loggedIn: user !== undefined, user, employee, client }}
     >
       {props.children}
     </AuthContext.Provider>
