1203463273b79771bfa57694e4f0b3c7af408000
Alex Maingot <alex.maingot@gmail.com>
2020-05-25 15:10:51 -0500

    Fix auth

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -3,7 +3,9 @@ import config from "./config";
 
 const app = admin.initializeApp({
   projectId: config.get("GCP_PROJECT_ID"),
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(
+    JSON.parse(config.get("GCP_SERVICE_ACCOUNT_KEY"))
+  ),
 });
 
 const TENANT_ID = config.get("GCP_IDP_TENANT_ID");
