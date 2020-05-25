8a0ba9a4b3d19fdf3447a16533811156783aa503
Alex Maingot <alex.maingot@gmail.com>
2020-05-25 08:43:01 -0500

    Fix static rendering

--- server/src/utils/renderHtml.ts
+++ server/src/utils/renderHtml.ts
@@ -11,19 +11,18 @@ const renderHtml = (app: Application) => {
       ? path.join(__dirname, "../../assets")
       : path.join(__dirname, "../../../web/build");
 
-  app.use(express.static(assetPath));
-
-  const rawHtml = fs.readFileSync(`${assetPath}/index.html`, "utf8");
-
-  // Config values for client
-  // const MAPBOX_TOKEN = config.get("MAPBOX_TOKEN");
-
-  const html = Mustache.render(rawHtml, {
-    // MAPBOX_TOKEN,
-  });
+  app.use(express.static(assetPath, { index: false }));
 
   app.get("/*", (_req, res) => {
-    res.send(html);
+    const rawHtml = fs.readFileSync(`${assetPath}/index.html`, "utf8");
+
+    const html = Mustache.render(rawHtml, {
+      TENANT_ID: config.get("GCP_IDP_TENANT_ID"),
+      FIREBASE_CONFIG: config.get("FIREBASE_APP_CONFIG").split('"').join('\\"'),
+      STRIPE_KEY: config.get("STRIPE_PUBLIC_KEY"),
+    });
+
+    res.status(200).send(html);
   });
 };
 
