4288280e45033c08d8ab0a9c44cd45cd4f7bb2e2
Alex Maingot <alex.maingot@gmail.com>
2021-07-08 21:35:07 +0000

    Remove support for HTTPS

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,6 @@ namespace TennisShopGuru
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
-      app.UseHttpsRedirection();
       app.UseStaticFiles();
 
       app.UseRouting();
